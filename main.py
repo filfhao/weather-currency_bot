b6cbd61c2b2785c3d912f5f072816dff2fd60afb
Kateryna Stetsiuk <katerina.stetsiukk@gmail.com>
2026-09-14 16:10:33 +0300

    Removed unused line

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     print('Starting bot...')
     app = Application.builder().token(TOKEN).build()
 
-    print(get_currency_rate("gbp"))
+
     app.add_handler(CommandHandler('start',start_command))
     app.add_handler(CommandHandler('help',help_command))
     app.add_handler(CommandHandler('custom',custom_command))
